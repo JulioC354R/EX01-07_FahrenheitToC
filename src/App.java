54a0b248fa9bf94cf811b5ddfb165b11021cea61
JulioC354R <julyocesarplay@gmail.com>
2023-04-04 22:13:20 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int Fahrenheit;
+        int celsius;
+
+         System.out.println("Insira a temperatura em Fahrenheit:");
+
+         Scanner teclado = new Scanner(System.in);
+        Fahrenheit = teclado.nextInt();
+    
+        celsius = 5 * ((Fahrenheit-32) / 9);
+
+
+        System.out.println(Fahrenheit + "F" + " é igual à " +  celsius + "C");
+
+    }
+}
